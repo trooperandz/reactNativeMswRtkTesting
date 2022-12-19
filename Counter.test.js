374f75b124d01600daf0b7d259f96a1cccbbbcc1
Matthew Holland <mtholland10@gmail.com>
2022-12-19 13:28:15 -0600

    Add successful waitFor with real api data fetch data

--- Counter.test.js
+++ Counter.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, screen, waitFor} from '@testing-library/react-native';
 import {Provider} from 'react-redux';
 
 import {store} from './store';
@@ -8,8 +8,11 @@ import {Counter} from './Counter';
 
 // jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
 
+const plot =
+  'Thinking he can overshadow an unknown actress in the part, an egocentric actor unknowingly gets a witch cast in an upcoming television remake of the classic sitcom Bewitched (1964).';
+
 describe('App', () => {
-  it('fetches correctly', () => {
+  it('fetches correctly', async () => {
     const {getByText} = render(
       <Provider store={store}>
         <Counter />
@@ -17,5 +20,10 @@ describe('App', () => {
     );
 
     expect(getByText('no data received yet')).toBeDefined();
+
+    // Note: this works without mocking; does the actual request!
+    await waitFor(() => screen.findByText(plot));
+
+    expect(plot).toBeDefined();
   });
 });
